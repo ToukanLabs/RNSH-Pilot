cfbe93dbf5e094dbeea706568e9552f7e843119e
Mike Leonard <mike.r.leonard@gmail.com>
2015-12-21 19:48:58 +1100

    Only try to require the CSS if we are in dev mode.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-require('../static/css/style.css');
+if (process.env.NODE_ENV !== 'production') {
+  require('../static/css/style.css');
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
