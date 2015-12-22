a0354fab91797d89e6f2292896c4e5d1f3bce5ff
TEEEEGYYYY <teighe.purnell@fivium.co.uk>
2015-12-22 18:57:45 +1100

    Staged Changes for Patient Header and menus as a shell

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -24,20 +24,6 @@ export class HomeView extends React.Component {
   render () {
     return (
       <div className='container text-center'>
-        <h1>Welcome to the React Redux Starter Kit</h1>
-        <h2>
-          Sample Counter:&nbsp;
-          <span className={styles['counter--green']}>{this.props.counter}</span>
-        </h2>
-        <button className='btn btn-default'
-                onClick={() => this.props.increment(1)}>
-          Increment
-        </button>
-        <button className='btn btn-default'
-                onClick={this.props.doubleAsync}>
-          Double (Async)
-        </button>
-        <hr />
         <Link to='/about'>Go To About View...</Link>
         <Link to='/patient/7'>Patients ...</Link>
         <br/>
