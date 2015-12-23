f754495d6466e59f0f42691fafbe0e3f5fa3abab
Mike Leonard <mike.r.leonard@gmail.com>
2015-12-23 13:20:03 +1100

    Added a home link.

--- src/components/GlobalHeader.js
+++ src/components/GlobalHeader.js
@@ -3,10 +3,15 @@ import GlobalSearch from 'components/GlobalSearch';
 import GlobalMenu from 'components/GlobalMenu';
 import UserMenu from 'components/UserMenu';
 import styles from './GlobalHeader.scss';
+import FontAwesome from 'react-fontawesome';
+import { Link } from 'react-router';
 
 export default class GlobalHeader extends Component {
   render () {
     return <div className={styles['gh-border']}>
+      <Link to="/">
+        <FontAwesome name='home' />
+      </Link>
       <GlobalMenu />
       <GlobalSearch icon='search' placeholder='Global Search'/>
       <UserMenu icon='fa fa-smile-o'/>
