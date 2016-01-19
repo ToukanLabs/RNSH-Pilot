5a6fa8124934592efb671813d6a703701fceb72d
TEEEEGYYYY <teighe.purnell@fivium.co.uk>
2016-01-19 14:06:59 +1100

    WIP for global search to get some needed pulls

--- src/components/GlobalSearch.js
+++ src/components/GlobalSearch.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import styles from './GlobalSearch.scss';
 
 export default class GlobalSearch extends Component {
+  constructor () {
+    super();
+    this.somefield = 'hello';
+
+    this.handleOnBlur = this.handleOnBlur.bind(this);
+  }
+
+  handleOnBlur () {
+    console.log('left search input');
+    // alert(this.somefield);
+  }
+
   render () {
     return (
       <div className={styles['gs-input-wrapper']}>
         <input
           placeholder={this.props.placeholder}
           className={styles['gs-input']}
-          />
+          onKeyPress={() => {
+          //  alert('poo');\
+            console.log('typing');
+          }
+        }
+          onBlur={this.handleOnBlur}
+        />
+        <div className={styles['gs-result-container']}>
+        </div>
       </div>
     );
   };
