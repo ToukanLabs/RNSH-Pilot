577c5efba885bace3f6d1a755d456fef69150ca7
Mike Leonard <mike.r.leonard@gmail.com>
2016-01-28 16:35:36 +1100

    Use SURNAME, forename format for patient search results.

--- src/components/SearchResultRow.js
+++ src/components/SearchResultRow.js
@@ -6,26 +6,28 @@ import styles from './SearchResultRow.scss';
 export default class SearchResultRow extends Component {
 
   render () {
+    const patient = this.props.patient;
+    const title = (patient.gender === 'F') ? 'Mrs' : 'Mr';
     return (
-      <Link to={`/patient/${this.props.patient.id}`}>
-        <li className={styles['gs-patient-search-result']} key={this.props.patient.id}>
+      <Link to={`/patient/${patient.id}`}>
+        <li className={styles['gs-patient-search-result']} key={patient.id}>
           <h2>
-            {this.props.patient.firstname + ' ' + this.props.patient.surname}
-            <span className={styles['sr-tumortype']}>({this.props.patient.tumorType})</span>
+            {`${patient.surname.toUpperCase()}, ${patient.firstname} (${title})`}
+            <span className={styles['sr-tumortype']}>({patient.tumorType})</span>
           </h2>
           <label htmlFor='patient-mrn' className={styles['sr-label']}>MRN: </label>
-          <span id='patient-mrn' className={styles['sr-value']}>{this.props.patient.mrn}</span>
+          <span id='patient-mrn' className={styles['sr-value']}>{patient.mrn}</span>
           <label htmlFor='patient-dob' className={styles['sr-label']}>DOB: </label>
           <span id='patient-dob' className={styles['sr-value']}>
             <FormattedDate
-              value={Date.parse(this.props.patient.dob)}
+              value={Date.parse(patient.dob)}
               day='numeric'
               month='short'
               year='numeric'
               />
           </span>
           <label htmlFor='patient-gender' className={styles['sr-label']}>Gender: </label>
-          <span id='patient-gender' className={styles['sr-value']}>{this.props.patient.gender}</span>
+          <span id='patient-gender' className={styles['sr-value']}>{patient.gender}</span>
         </li>
       </Link>
     );
