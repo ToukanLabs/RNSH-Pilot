affa2b53ca30f71accf0466ad1c028d819b92f41
purnet <teigheyyy@gmail.com>
2016-02-17 15:49:13 +1100

    dev tools filter installed and ready to use with a couple of annoying ones already removed for redux-form and our update_search_string

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
+import FilterMonitor from 'redux-devtools-filter-actions';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
@@ -7,6 +8,8 @@ export default createDevTools(
   <DockMonitor
     toggleVisibilityKey='ctrl-h'
     changePositionKey='ctrl-q' >
-    <LogMonitor />
+    <FilterMonitor blacklist={['UPDATE_SEARCH_STRING', 'redux-form/CHANGE', 'redux-form/BLUR', 'redux-form/FOCUS']}>
+      <LogMonitor />
+    </FilterMonitor>
   </DockMonitor>
 );
--- src/containers/DevToolsWindow.js
+++ src/containers/DevToolsWindow.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
+import FilterMonitor from 'redux-devtools-filter-actions';
 
 export default createDevTools(
-  <LogMonitor />
+  <FilterMonitor blacklist={['UPDATE_SEARCH_STRING', 'redux-form/CHANGE', 'redux-form/BLUR', 'redux-form/FOCUS']}>
+    <LogMonitor />
+  </FilterMonitor>
 );
