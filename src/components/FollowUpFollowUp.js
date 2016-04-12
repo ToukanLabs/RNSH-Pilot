d550d5e543c5ab9277e537de91b6ee6864c4c5c8
Rob Gwynn-Jones <gwynnr@gmail.com>
2016-04-13 09:51:45 +1000

    Revamp FollowUpFollowUp layout and styles

--- src/components/FollowUpFollowUp.js
+++ src/components/FollowUpFollowUp.js
@@ -106,7 +106,7 @@ export class FollowUpFollowUp extends Component {
     const saveControls = () => {
       if (this.props.enableFollowUpSave) {
         return (
-          <div>
+          <div className={styles.saveControls}>
             <button type='submit' disabled={submitting}>
               {submitting ? <i/> : <i/>} Save
             </button>
@@ -132,29 +132,26 @@ export class FollowUpFollowUp extends Component {
       } = this.props;
 
     return (
-      <Panel title={this.props.panelTitle} className={this.props.className}>
-        <form key={id} className={styles.apvForm} onSubmit={handleSubmit(this.submit)}>
-          <div className={styles.fufuRowOne}>
-            <div className={styles.fufuTopLeftContainer}>
-              <div className={styles.fufuTlcOne}>
-                <DateTimeInput
-                  label='FU Date'
-                  mandatory
-                  noTime
-                  formField={date}
-                  />
-                <TextInput
-                  label='Time from Prostate RT Finished'
-                  unitLabel='months'
-                  formField={prostateRTFinished}
-                  />
-                <TextInput
-                  label='Time from Last Course of RT'
-                  unitLabel='months'
-                  formField={lastCourseRT}
-                  />
-              </div>
-              <div className={styles.fufuTlcTwo}>
+      <form key={id} className={styles.apvForm} onSubmit={handleSubmit(this.submit)}>
+        <div className={styles.fufuRowOneContainer}>
+          <Panel title={this.props.panelTitle} className={this.props.className}>
+            <div className={styles.fufuTlcOne}>
+              <DateTimeInput
+                label='FU Date'
+                mandatory
+                noTime
+                formField={date}
+                />
+              <TextInput
+                label='Time from Prostate RT Finished'
+                unitLabel='months'
+                formField={prostateRTFinished}
+                />
+              <TextInput
+                label='Time from Last Course of RT'
+                unitLabel='months'
+                formField={lastCourseRT}
+                />
                 <Select
                   {...hormones}
                   value={hormones.value || ''}
@@ -179,53 +176,52 @@ export class FollowUpFollowUp extends Component {
                   ]}
                   />
                 <InlineWidgetGroup>
-                  <Select
-                    {...alphaBlocker}
-                    value={alphaBlocker.value || ''}
-                    label='Alpha Blocker / Anticholinergic'
-                    labelClassName={styles.fufuAlphaBlockerLabel}
-                    options={[
-                      {key: 'Yes', value: 'Yes'},
-                      {key: 'No', value: 'No'},
-                    ]}
-                    />
-                  {' - '}
-                  {anticholinergicWidget(anticholinergic)}
+                  <div className={styles.fufuAlphaBlockerContainer}>
+                    <Select
+                      {...alphaBlocker}
+                      value={alphaBlocker.value || ''}
+                      label='Alpha Blocker / Anticholinergic'
+                      labelClassName={styles.fufuAlphaBlockerLabel}
+                      options={[
+                        {key: 'Yes', value: 'Yes'},
+                        {key: 'No', value: 'No'},
+                      ]}
+                      />
+                    {' - '}
+                    {anticholinergicWidget(anticholinergic)}
+                  </div>
                 </InlineWidgetGroup>
-              </div>
-
-              <div className={styles.fufuSubPanelOne}>
-                <Select
-                  {...currentFU}
-                  value={currentFU.value || ''}
-                  label='Current Follow-Up'
-                  options={[
-                    {key: 'Phone', value: 'Phone'},
-                    {key: 'In-Person', value: 'In-Person'},
-                  ]}
-                  />
-                <FollowUpDoctorSelect
-                  label='Doctor'
-                  formValue={doctor}
-                  />
-                <Select
-                  {...nocturia}
-                  value={nocturia.value || ''}
-                  label='Nocturia'
-                  options={[
-                    {key: '0', value: '0'},
-                    {key: '1', value: '1'},
-                    {key: '2', value: '2'},
-                    {key: '3', value: '3'},
-                    {key: '4', value: '4'},
-                    {key: '5', value: '5'},
-                    {key: '6', value: '6'},
-                    {key: '>=6', value: '>=6'},
-                    {key: 'NA', value: 'NA'},
-                  ]}
-                  />
-              </div>
-              <div className={styles.fufuSubPanelTwo}>
+            </div>
+            <div className={styles.fufuTlcTwo}>
+              <Select
+                {...currentFU}
+                value={currentFU.value || ''}
+                label='Current Follow-Up'
+                options={[
+                  {key: 'Phone', value: 'Phone'},
+                  {key: 'In-Person', value: 'In-Person'},
+                ]}
+                />
+              <FollowUpDoctorSelect
+                label='Doctor'
+                formValue={doctor}
+                />
+              <Select
+                {...nocturia}
+                value={nocturia.value || ''}
+                label='Nocturia'
+                options={[
+                  {key: '0', value: '0'},
+                  {key: '1', value: '1'},
+                  {key: '2', value: '2'},
+                  {key: '3', value: '3'},
+                  {key: '4', value: '4'},
+                  {key: '5', value: '5'},
+                  {key: '6', value: '6'},
+                  {key: '>=6', value: '>=6'},
+                  {key: 'NA', value: 'NA'},
+                ]}
+                />
                 <Select
                   {...biochemicalFailure}
                   value={biochemicalFailure.value || ''}
@@ -240,117 +236,130 @@ export class FollowUpFollowUp extends Component {
                 {metastasesWidget(metastases)}
 
                 {metastasesSiteAndDateWidgets(site, metastasesDate)}
-              </div>
             </div>
+          </Panel>
+        </div>
 
-            <div className={styles.fufuTopMiddleContainer}>
-              <div className={styles.fufuQolContainer}>
-                <h3 className={styles.fufuSubHeadingTop}>Quality of Life</h3>
-                <Select
-                  {...epicCompleted}
-                  value={epicCompleted.value || ''}
-                  label='EPIC Completed'
-                  options={[
-                    {key: 'Yes', value: 'Yes'},
-                    {key: 'No', value: 'No'},
-                    {key: 'Yes, before RT', value: 'Yes, before RT'},
-                    {key: 'Yes, completed but paperwork lost', value: 'Yes, completed but paperwork lost'},
-                  ]}
+        <div className={styles.fufuRowTwoContainer}>
+          <div className={styles.fufuQolContainer}>
+            <Panel title={"Quality of Life"}>
+              <Select
+                {...epicCompleted}
+                value={epicCompleted.value || ''}
+                label='EPIC Completed'
+                options={[
+                  {key: 'Yes', value: 'Yes'},
+                  {key: 'No', value: 'No'},
+                  {key: 'Yes, before RT', value: 'Yes, before RT'},
+                  {key: 'Yes, completed but paperwork lost', value: 'Yes, completed but paperwork lost'},
+                ]}
+                />
+              <InlineWidgetGroup>
+                <TextInput
+                  label='IPSS'
+                  className={styles.fufuQOLSmallField}
+                  formField={ipssOne}
                   />
-                <InlineWidgetGroup>
-                  <TextInput
-                    label='IPSS'
-                    formField={ipssOne}
-                    />
-                  +
-                  <TextInput formField={ipssTwo}/>
-                </InlineWidgetGroup>
-                <InlineWidgetGroup>
-                  <TextInput
-                    label='SF-12 PCS'
-                    className={styles.fufuSf}
-                    formField={sf12pcs}
-                    />
-                  <TextInput
-                    label='SF-12 MCS'
-                    className={styles.fufuSf}
-                    formField={sp12mcs}
-                    />
-                </InlineWidgetGroup>
-                <div className={styles.fufuDataimissingInfo}>
-                  ("-1" = data missing / NA)
-                </div>
+                {' +'}
+                <TextInput
+                  className={styles.fufuQOLSmallField}
+                  formField={ipssTwo}
+                  />
+              </InlineWidgetGroup>
+              <InlineWidgetGroup>
+                <TextInput
+                  label='SF-12 PCS'
+                  className={styles.fufuQOLSmallField}
+                  formField={sf12pcs}
+                  />
+                {' SF-12 MCS'}
+                <TextInput
+                  className={styles.fufuQOLSmallField}
+                  formField={sp12mcs}
+                  />
+              </InlineWidgetGroup>
+              <div className={styles.fufuDataimissingInfo}>
+                ("-1" = data missing / NA)
+              </div>
+              <div className={styles.fufuQolButtonContainer}>
                 <button>Get EPIC Scores</button>
                 <button>Enter EPIC Data</button>
               </div>
-
-              <div className={styles.fufuHrqolContainer}>
-                <h3>HRQOL Domain Scores</h3>
+            </Panel>
+          </div>
+          <div className={styles.fufuHrqolToxicityContainer}>
+            <div className={styles.fufuHrqolContainer}>
+              <Panel title={"HRQOL Domain Scores"}>
                 <InlineWidgetGroup>
                   <TextInput
                     label='Urinary'
+                    className={styles.fufuQOLSmallField}
                     formField={hrqolUrinary}
                     />
                   <TextInput
                     label='Sexual'
+                    className={styles.fufuQOLSmallField}
                     formField={hrqolSexual}
                     />
-                </InlineWidgetGroup>
-                <InlineWidgetGroup>
                   <TextInput
                     label='Bowel'
+                    className={styles.fufuQOLSmallField}
                     formField={hrqolBowel}
                     />
                   <TextInput
                     label='Hormonal'
+                    className={styles.fufuQOLSmallField}
                     formField={hrqolHormonal}
                     />
                 </InlineWidgetGroup>
-                <TextInput
-                  label='Record ID in EPIC DB'
-                  disabled
-                  />
-                <button>View All EPIC Data</button>
-              </div>
-            </div>
-
-            <div className={styles.fufuTopRightContainer}>
-              <h3 className={styles.fufuSubHeadingTop}>Toxicity Assessment</h3>
-              <button>Open Assessment Form</button>
-              <button>Check All Assessments</button>
-            </div>
-          </div>
-
-          <div className={styles.fufuRowTwoContainer}>
-            <div className={styles.fufuRowTwo}>
-              <div className={styles.fufuSecondCancerContainer}>
                 <InlineWidgetGroup>
-                  <Select
-                    {...secondCancer}
-                    label='2nd Cancer'
-                    options={[
-                      {key: 'Yes', value: 'Yes'},
-                      {key: 'No', value: 'No'},
-                    ]}
-                    />
                   <TextInput
-                    label='Date/Histology'
-                    formField={dateHistology}
+                    label='Record ID in EPIC DB'
+                    className={styles.fufuQOLSmallField}
+                    disabled
                     />
+                  <button>View All EPIC Data</button>
                 </InlineWidgetGroup>
-              </div>
-              <div className={styles.fufuCommentsContainer}>
-                <TextArea
-                  label='Comments'
-                  {...comments}
-                  value={comments.value || ''}
-                  />
-              </div>
+              </Panel>
+            </div>
+            <div className={styles.fufuToxicityContainer}>
+              <Panel title={"Toxicity Assessment"}>
+                <button>Open Assessment Form</button>
+                <button>Check All Assessments</button>
+              </Panel>
             </div>
           </div>
-          {saveControls()}
-        </form>
-      </Panel>
+        </div>
+
+        <div className={styles.fufuRowThreeContainer}>
+          <Panel>
+            <div className={styles.fufuSecondCancerContainer}>
+              <InlineWidgetGroup>
+                <Select
+                  {...secondCancer}
+                  label='2nd Cancer'
+                  options={[
+                    {key: 'Yes', value: 'Yes'},
+                    {key: 'No', value: 'No'},
+                  ]}
+                  />
+                <TextInput
+                  label='Date/Histology'
+                  formField={dateHistology}
+                  />
+              </InlineWidgetGroup>
+            </div>
+            <div className={styles.fufuCommentsContainer}>
+              <TextArea
+                label='Comments'
+                {...comments}
+                value={comments.value || ''}
+                />
+            </div>
+            {saveControls()}
+          </Panel>
+        </div>
+      </form>
     );
   };
 };
